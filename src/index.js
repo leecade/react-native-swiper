d4a23c6817b6592bba171240089a1820e592732a
Alma Madsen <madsenat@gmail.com>
2016-09-30 10:21:06 -0700

    Fix jitter when offset is set while swiping quickly back and forth.

--- src/index.js
+++ src/index.js
@@ -149,7 +149,7 @@ export default class extends Component {
    * Init states
    * @return {object} states
    */
-  state = this.initState(this.props)
+  state = this.initState(this.props, true)
 
   /**
    * autoplay timer
@@ -158,12 +158,18 @@ export default class extends Component {
   autoplayTimer = null
   loopJumpTimer = null
 
-  componentWillReceiveProps (props) {
-    this.setState(this.initState(props))
+  componentWillReceiveProps (nextProps) {
+    const sizeChanged = (nextProps.width || width) !== this.state.width ||
+                        (nextProps.height || height) !== this.state.height
+    this.setState(this.initState(nextProps, sizeChanged))
   }
 
   componentDidMount () {
     this.autoplay()
+  }
+
+  shouldComponentUpdate () {
+    return !this.internals.isScrolling
   }
 
   componentWillUnmount () {
@@ -171,14 +177,17 @@ export default class extends Component {
     this.loopJumpTimer && clearTimeout(this.loopJumpTimer)
   }
 
-  initState (props) {
+  initState (props, setOffsetInState) {
     // set the current state
     const state = this.state || {}
 
-    let initState = {
-      isScrolling: false,
+    const initState = {
       autoplayEnd: false,
       loopJump: false
+    }
+
+    const newInternals = {
+      isScrolling: false
     }
 
     initState.total = props.children ? props.children.length || 1 : 0
@@ -195,18 +204,31 @@ export default class extends Component {
     initState.dir = props.horizontal === false ? 'y' : 'x'
     initState.width = props.width || width
     initState.height = props.height || height
-    initState.offset = {}
+    newInternals.offset = {}
 
     if (initState.total > 1) {
       let setup = initState.index
       if (props.loop) {
         setup++
       }
-      initState.offset[initState.dir] = initState.dir === 'y'
+      newInternals.offset[initState.dir] = initState.dir === 'y'
         ? initState.height * setup
         : initState.width * setup
     }
+
+    // only update the offset in state if needed, updating offset while swiping
+    // causes some bad jumping / stuttering
+    if (setOffsetInState) {
+      initState.offset = newInternals.offset
+    }
+
+    this.internals = newInternals
     return initState
+  }
+
+  // include internals with state
+  fullState () {
+    return Object.assign({}, this.state, this.internals)
   }
 
   loopJump = () => {
@@ -223,7 +245,7 @@ export default class extends Component {
   autoplay = () => {
     if (!Array.isArray(this.props.children) ||
       !this.props.autoplay ||
-      this.state.isScrolling ||
+      this.internals.isScrolling ||
       this.state.autoplayEnd) return
 
     this.autoplayTimer && clearTimeout(this.autoplayTimer)
@@ -245,9 +267,8 @@ export default class extends Component {
    */
   onScrollBegin = e => {
     // update scroll state
-    this.setState({ isScrolling: true }, () => {
-      this.props.onScrollBeginDrag && this.props.onScrollBeginDrag(e, this.state, this)
-    })
+    this.internals.isScrolling = true
+    this.props.onScrollBeginDrag && this.props.onScrollBeginDrag(e, this.fullState(), this)
   }
 
   /**
@@ -256,9 +277,7 @@ export default class extends Component {
    */
   onScrollEnd = e => {
     // update scroll state
-    this.setState({
-      isScrolling: false
-    })
+    this.internals.isScrolling = false
 
     // making our events coming from android compatible to updateIndex logic
     if (!e.nativeEvent.contentOffset) {
@@ -285,15 +304,14 @@ export default class extends Component {
   onScrollEndDrag = e => {
     const { contentOffset } = e.nativeEvent
     const { horizontal, children } = this.props
-    const { offset, index } = this.state
+    const { index } = this.state
+    const { offset } = this.internals
     const previousOffset = horizontal ? offset.x : offset.y
     const newOffset = horizontal ? contentOffset.x : contentOffset.y
 
     if (previousOffset === newOffset &&
       (index === 0 || index === children.length - 1)) {
-      this.setState({
-        isScrolling: false
-      })
+      this.internals.isScrolling = false
     }
   }
 
@@ -305,7 +323,7 @@ export default class extends Component {
   updateIndex = (offset, dir, cb) => {
     const state = this.state
     let index = state.index
-    const diff = offset[dir] - state.offset[dir]
+    const diff = offset[dir] - this.internals.offset[dir]
     const step = dir === 'x' ? state.width : state.height
     let loopJump = false
 
@@ -329,9 +347,9 @@ export default class extends Component {
       }
     }
 
+    this.internals.offset = offset
     this.setState({
       index: index,
-      offset: offset,
       loopJump: loopJump
     }, cb)
   }
@@ -341,7 +359,7 @@ export default class extends Component {
    * @param  {number} index offset index
    */
   scrollBy = index => {
-    if (this.state.isScrolling || this.state.total < 2) return
+    if (this.internals.isScrolling || this.state.total < 2) return
     const state = this.state
     const diff = (this.props.loop ? 1 : 0) + index + this.state.index
     let x = 0
@@ -356,8 +374,8 @@ export default class extends Component {
     }
 
     // update scroll state
+    this.internals.isScrolling = true
     this.setState({
-      isScrolling: true,
       autoplayEnd: false
     })
 
