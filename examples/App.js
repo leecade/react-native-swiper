8728eef3fc6c1f5cabf7addfa28695af66f13c0f
Charleston Malkemus <cgmalkemus@gmail.com>
2017-01-13 15:22:42 -0500

    Cleaned up and tested example

--- examples/App.js
+++ examples/App.js
@@ -1,10 +1,10 @@
 import { AppRegistry } from 'react-native'
-// import Basic from './components/Basic/'
+import Basic from './components/Basic/'
 // import Dynamic from './components/Dynamic/'
 // import Phone from './components/Phone/'
 // import Swiper from './components/Swiper/'
 // import SwiperNumber from './components/SwiperNumber/'
 // import LoadMinimal from './components/LoadMinimal/'
 // import PhotoView from './components/PhotoView/'
-import Header from './components/Header/'
+// import Header from './components/Header/'
 AppRegistry.registerComponent('examples', () => Header)
