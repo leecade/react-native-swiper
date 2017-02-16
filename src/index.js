430cb5de6a3529cf10005d6a9ca202ec055608a2
Julian Kingman <julian@designman.io>
2017-02-16 08:58:33 -0500

    added container style for wrapping view

--- src/index.js
+++ src/index.js
@@ -100,6 +100,7 @@ export default class extends Component {
   static propTypes = {
     horizontal: PropTypes.bool,
     children: PropTypes.node.isRequired,
+    containerStyle: View.propTypes.style,
     style: View.propTypes.style,
     pagingEnabled: PropTypes.bool,
     showsHorizontalScrollIndicator: PropTypes.bool,
@@ -628,7 +629,7 @@ export default class extends Component {
       <View style={[styles.container, {
         width: state.width,
         height: state.height
-      }]}>
+      }, props.containerStyle]}>
         {this.renderScrollView(pages)}
         {props.showsPagination && (props.renderPagination
           ? this.props.renderPagination(state.index, state.total, this)
