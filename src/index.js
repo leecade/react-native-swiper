664c41dbaecded5372f9dcab02e6b0298c9ec3bd
Martin Adamko <attitude@users.noreply.github.com>
2018-01-23 14:50:39 +0100

    Hotfix undefined refScrollView
    Fixes `this.scrollView`being undefined on some occasions.

--- src/index.js
+++ src/index.js
@@ -294,7 +294,7 @@ export default class extends Component {
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
     if (Platform.OS === 'ios') {
-      if (this.initialRender && this.state.total > 1) {
+      if (this.initialRender && this.state.total > 1 && this.scrollView) {
         this.scrollView.scrollTo({...offset, animated: false})
         this.initialRender = false;
       }
@@ -307,7 +307,7 @@ export default class extends Component {
     if (!this.state.loopJump) return
     const i = this.state.index + (this.props.loop ? 1 : 0)
     const scrollView = this.scrollView
-    this.loopJumpTimer = setTimeout(() => scrollView.setPageWithoutAnimation &&
+    this.loopJumpTimer = setTimeout(() => scrollView && scrollView.setPageWithoutAnimation &&
       scrollView.setPageWithoutAnimation(i), 50)
   }
 
