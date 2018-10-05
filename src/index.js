69e1a3a597581346a7e4cfb1ed5e609785286506
Alam Yudi Aryanto <alamyudi@Alams-MacBook-Pro-2.local>
2018-10-05 09:53:03 +0700

    set state if current props and next props is different

--- src/index.js
+++ src/index.js
@@ -195,7 +195,9 @@ export default class extends Component {
 
   componentWillReceiveProps (nextProps) {
     if (!nextProps.autoplay && this.autoplayTimer) clearTimeout(this.autoplayTimer)
-    this.setState(this.initState(nextProps, this.props.index !== nextProps.index))
+    if (this.props.index !== nextProps.index) {
+      this.setState(this.initState(nextProps, this.props.index !== nextProps.index))
+    }
   }
 
   componentDidMount () {
