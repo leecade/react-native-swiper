2cbaad9b492c237af8938bfa80cb811ad1897645
guillaume rean <rean.guillaume@gmail.com>
2019-05-19 10:22:47 +0200

    Change ViewPagerAndroid to ViewPager from @react-native-community/viewpager

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ import {
   ScrollView,
   Dimensions,
   TouchableOpacity,
-  ViewPagerAndroid,
   Platform,
   ActivityIndicator
 } from 'react-native'
+import ViewPager from "@react-native-community/viewpager";
 
 /**
  * Default styles
@@ -649,7 +649,7 @@ export default class extends Component {
        )
     }
     return (
-      <ViewPagerAndroid ref={this.refScrollView}
+      <ViewPager ref={this.refScrollView}
         {...this.props}
         initialPage={this.props.loop ? this.state.index + 1 : this.state.index}
         onPageScrollStateChanged={this.onPageScrollStateChanged}
@@ -657,7 +657,7 @@ export default class extends Component {
         key={pages.length}
         style={[styles.wrapperAndroid, this.props.style]}>
         {pages}
-      </ViewPagerAndroid>
+      </ViewPager>
     )
   }
 
