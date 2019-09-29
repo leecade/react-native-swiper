802edd1ca3edce3ade884e22e2935b5d32289931
ArrayZoneYour <hustliyidong@gmail.com>
2019-09-29 18:41:28 +0800

    chore(npm): bump the version

--- examples/index.js
+++ examples/index.js
@@ -9,7 +9,7 @@ import Basic from './components/Basic'; // Only update index when scrollEnd
 import DisableButton from './components/DisableButton';
 import Loop from './components/Loop';
 import Dynamic from './components/Dynamic/';
-import LoadMinimal from './components/LoadMinimal/';
+import LoadMinimal from './components/LoadMinimal/'; // flicker existing when shift from last to first on Android (or last to first)
 import Phone from './components/Phone/';
 // import PhotoView from './components/PhotoView/'; // not working
 import Swiper from './components/Swiper/'; // working but no title displayed, direction vertical not work well on android
