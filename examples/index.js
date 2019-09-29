3a119c845443721d8aef4a0d51d398eb478fca03
ArrayZoneYour <hustliyidong@gmail.com>
2019-09-29 14:30:09 +0800

    chore(example): provide DisableButton example
    re #1073

--- examples/index.js
+++ examples/index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
 import Basic from './components/Basic'; // Only update index when scrollEnd
+import DisableButton from './components/DisableButton';
 import Loop from './components/Loop';
 import Dynamic from './components/Dynamic/';
 import LoadMinimal from './components/LoadMinimal/'; // flicker existing when shift from last to first (or last to first)
@@ -13,4 +14,4 @@ import Phone from './components/Phone/';
 // import PhotoView from './components/PhotoView/'; // not working
 import Swiper from './components/Swiper/'; // working but no title displayed, direction vertical not work well on android
 import SwiperNumber from './components/SwiperNumber/'; // working but no title displayed
-AppRegistry.registerComponent(appName, () => Basic);
+AppRegistry.registerComponent(appName, () => DisableButton);
