e1c3cd5df4420bc6c6f2cb33399608e78a6e8326
B.Isberner <git@trexpert-it.com>
2020-01-14 16:50:12 +0100

    scroll to new position if swipe container size chages (ex: orientation change)

--- src/index.js
+++ src/index.js
@@ -321,8 +321,11 @@ export default class extends Component {
     // related to https://github.com/leecade/react-native-swiper/issues/570
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
-    if (this.initialRender && this.state.total > 1) {
+    if(this.state.total > 1) {
       this.scrollView.scrollTo({ ...offset, animated: false })
+    }
+	
+    if (this.initialRender) {
       this.initialRender = false
     }
 
